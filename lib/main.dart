70981f5a9004a5015360d66580413207f2307c73
cankush625 <cankush625@gmail.com>
2020-08-06 23:26:01 +0530

    Integrating flutter with linux server on AWS
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
-import 'Flutter_Linux_Integration/demo.dart';
+import 'Flutter_Linux_Integration/demo1.dart';
 
 void main() {
   runApp(BizApp());
