2c71795a0cf7a1bbfb1c4a43fa051127c227369a
cankush625 <cankush625@gmail.com>
2020-08-18 23:59:34 +0530

    Google map api
    Signed-off-by: cankush625 <cankush625@gmail.com>

---
+++ lib/API/map.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import '';
+
+class Api extends StatefulWidget {
+  @override
+  _ApiState createState() => _ApiState();
+}
+// ignore: non_constant_identifier_names
+class _ApiState extends State<Api> {
+}
+}
